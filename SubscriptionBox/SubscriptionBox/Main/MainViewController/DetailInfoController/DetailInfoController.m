f2455dc23b5d7a3337cfb50884db3fa5bd40557c
canoe <781276284@qq.com>
2018-02-22 13:58:34 +0800

    新年快乐

---
+++ SubscriptionBox/SubscriptionBox/Main/MainViewController/DetailInfoController/DetailInfoController.m
@@ -0,0 +1,37 @@
+//
+//  DetailInfoController.m
+//  SubscriptionBox
+//
+//  Created by canoe on 2018/2/22.
+//  Copyright © 2018年 canoe. All rights reserved.
+//
+
+#import "DetailInfoController.h"
+
+@interface DetailInfoController ()
+
+@end
+
+@implementation DetailInfoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
